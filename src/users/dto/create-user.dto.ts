4a5ec136245d7c4d873b4a8adadd1bb800df9a9a
hissoune <khalidhissoune962@gmail.com>
2024-11-21 15:10:01 +0100

    Add : adding the dto to validate the user creation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUserDto {}
+import { IsString, IsEmail, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  password: string;
+}
