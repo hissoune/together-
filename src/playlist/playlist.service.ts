c97ddd37c74a57d7e2c14d6610b0b81627e2ad92
hissoune <khalidhissoune962@gmail.com>
2024-11-23 11:11:41 +0100

    Add : adding the function of creating and find all and injecting the model of the playlist

--- src/playlist/playlist.service.ts
+++ src/playlist/playlist.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePlaylistDto } from './dto/create-playlist.dto';
 import { UpdatePlaylistDto } from './dto/update-playlist.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Playlist, PlaylistDocument } from './schemas/playlist.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class PlaylistService {
+  constructor(@InjectModel(Playlist.name) private readonly playlistmodel:Model<PlaylistDocument>){}
   create(createPlaylistDto: CreatePlaylistDto) {
-    return 'This action adds a new playlist';
+
+    
+    const newplaylist = new  this.playlistmodel(createPlaylistDto)
+    
+    
+    return newplaylist.save();
   }
 
-  findAll() {
-    return `This action returns all playlist`;
+  findAll(ownerId:string) {
+    return this.playlistmodel.find({owner:ownerId});
   }
 
-  findOne(id: number) {
+  findOne(id: string,ownerId:string) {
     return `This action returns a #${id} playlist`;
   }
 
