4cd9e880817ce7d5613220cc52412d06eb5090f2
hissoune <khalidhissoune962@gmail.com>
2024-12-08 11:59:02 +0100

    Feature: Enhanced create-user.dto.ts to include new validation rules

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,13 +1,27 @@
-import { IsString, IsEmail, MinLength } from 'class-validator';
+import { IsEmail, IsOptional, IsString, MinLength } from "class-validator";
+
 
 export class CreateUserDto {
-  @IsString()
-  name: string;
+    @IsString()
+    @IsOptional()
+    name?: string;
+  
+    @IsEmail()
+    email: string;
+  
+    @IsString()
+    @MinLength(6, { message: 'Password must be at least 6 characters long' })
+    password: string;
 
-  @IsEmail()
-  email: string;
+    @IsString()
+    @IsOptional()
+    gender?:string
 
-  @IsString()
-  @MinLength(6, { message: 'Password must be at least 6 characters long' })
-  password: string;
+    @IsString()
+    @IsOptional()
+    role?:string
+
+    @IsString()
+    @IsOptional()
+    imagePath?:string
 }
