c4a931f9769f3986bfc2b8b1f09e6e209521d7c4
hissoune <khalidhissoune962@gmail.com>
2024-12-08 11:58:59 +0100

    Refactor: Updated app.module.ts to integrate new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,51 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import config  from './utils/config/config';
+import { AuthGuard } from './guards/auth.guard';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { User, userSchema } from './users/entities/user.entity';
 import { PlaylistModule } from './playlist/playlist.module';
+
 
 @Module({
   imports: [
+    MongooseModule.forFeature([{name:User.name,schema:userSchema}]),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'), 
+      serveRoot: '/uploads', 
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
+      cache:true,
+    load:[config]
     }),
 
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('jwt.secret'),
+      }),
+      global:true,
+      inject:[ConfigService]
+    }),
     MongooseModule.forRootAsync({
+
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGO_URI'),
+        uri: configService.get<string>('database.connectionString'),
       }),
       inject: [ConfigService],
     }),
-
     UsersModule,
-
-    PlaylistModule,
+    AuthModule,
+    PlaylistModule
   ],
   controllers: [],
-  providers: [],
+  providers: [AuthGuard],
 })
 export class AppModule {}
