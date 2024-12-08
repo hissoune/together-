e88fa0ff30eadaf6e1b6520f576e98135549b655
hissoune <khalidhissoune962@gmail.com>
2024-12-08 11:59:03 +0100

    Fix: Modified user.entity.ts to align with new schema

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,27 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema()
+export class User extends Document {
+
+@Prop({ required: true })
+  name: string;
+
+@Prop({ required: true, unique: true })
+  email: string;
+
+@Prop({enum:['man','woman'],default:'woman'})
+gender:string
+  
+@Prop({required:true})
+imagePath:string
+
+@Prop({ required: true })
+  password: string;
+
+@Prop({ required: true,enum:['participant','organizer'],default:"participant" })
+  role: string;
+
+}
+
+export const userSchema  = SchemaFactory.createForClass(User)
